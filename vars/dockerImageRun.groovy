b4541ca1b4728710a980ba1d9ddc4e611b08a5a0
aloui.wissem <alouiwiss@gmail.com>
2023-07-31 16:10:57 +0100

    Update dockerImageRun.groovy

--- vars/dockerImageRun.groovy
+++ vars/dockerImageRun.groovy
@@ -1,1 +1,4 @@
 
+def call(String project, String ImageTag, String portMapping) {
+    sh "docker container run -d -p ${portMapping} --name ${project}_${ImageTag} ${hubUser}/${project}:${ImageTag}"
+}
